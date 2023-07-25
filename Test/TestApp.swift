4d90b199633b12a34430b8eba01dd65fc4e0bd7f
GuiHoffmannDev <guihoffmann.dev@outlook.com>
2023-07-25 18:01:05 -0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Guilherme on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
